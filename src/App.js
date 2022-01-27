193f82ce2c551d9a8abb5c36206e7be3b20b36e7
ms-anastasia <nastyakorn0@gmail.com>
2022-01-27 19:24:40 +0200

    uses hooks

--- src/App.js
+++ src/App.js
@@ -1,65 +1,107 @@
-import React, { Component } from "react";
+import { useState, useEffect } from "react";
 import Container from "./components/Container/Container";
 import SearchBar from "./components/Searchbar/Searchbar";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
+import ImgApiService from "./services/api";
+import { Message } from "./components/ImageGallery/ImageGallery.styled";
+import {
+  ButtonWrapper,
+  LoadMoreButton,
+} from "./components/Button/Button.styled";
+import Spinner from "./components/Spinner/Spinner";
 import Modal from "./components/Modal/Modal";
 import { ModalCloseButton } from "./components/Modal/Modal.styled";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+export default function App() {
+  const [request, setRequest] = useState("");
+  const [largeImgSrc, setLargeImgSrc] = useState("");
+  const [showModal, setShowModal] = useState("");
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState("idle");
+  const [page, setPage] = useState(1);
 
-class App extends Component {
-  state = {
-    request: '',
-    largeImgSrc: '',
-    showModal: false,
-    largeImgSrc: ""
+  const handleFormSubmit = (request) => {
+    setRequest(request);
+    setImages([]);
   };
 
-  handleFormSubmit = request => {
-    this.setState({ request });
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+  useEffect(() => {
+    if (!request) {
+      return;
+    }
+    if (page === 1) {
+      setImages([]);
+      setStatus("idle");
+    }
+    setStatus("pending");
+    const imgApiService = new ImgApiService();
+    imgApiService.query = request;
+    imgApiService.page = page;
+    imgApiService
+      .fetchImg()
+      .then((data) => {
+        if (data.hits.length === 0) {
+          toast.error("Nothing found on your query!", {
+            theme: "colored",
+          });
+        }
+        setImages((images) => [...images, ...data.hits]);
+        setStatus("resolved");
+      })
+      .catch((error) => {
+        setError(error);
+        setStatus("rejected");
+      });
+  }, [request, page]);
+
+  const onLoadClick = () => {
+    setPage((state) => state + 1);
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
+  const onImgClick = (largeImageURL) => {
+    toggleModal();
+    setLargeImgSrc(largeImageURL);
   };
 
-  onImgClick = (largeImageURL) => {
-    this.toggleModal();
-    this.setState({ largeImgSrc: largeImageURL });
-  };
-    
-  render() {
-    return (
-      <Container>
-        <SearchBar onSubmit={this.handleFormSubmit}/>
-        <ImageGallery request={this.state.request} onClick={this.onImgClick} />
-        {this.state.showModal && (
-          <Modal onClose={this.toggleModal}>
-            <img
-              src={this.state.largeImgSrc}
-              alt=""
-              width="100%"
-              height="100%"
-            />
-            <ModalCloseButton onClick={this.toggleModal}>Close Modal</ModalCloseButton>
-          </Modal>
-        )}
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          />
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <SearchBar onSubmit={handleFormSubmit} />
+      {status === "idle" && <Message>Please enter your query</Message>}
+      {status === "pending" && <Spinner />}
+      {status === "rejected" && <div> {error.message} </div>}
+      {status === "resolved" && (
+        <ImageGallery images={images} onClick={onImgClick} />
+      )}
+      {images.length > 0 && images.length % 12 === 0 && (
+        <ButtonWrapper>
+          <LoadMoreButton type="button" onClick={onLoadClick}>
+            Load more
+          </LoadMoreButton>
+        </ButtonWrapper>
+      )}
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <img src={largeImgSrc} alt="" width="100%" height="100%" />
+          <ModalCloseButton onClick={toggleModal}>Close Modal</ModalCloseButton>
+        </Modal>
+      )}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </Container>
+  );
 }
-export default App;

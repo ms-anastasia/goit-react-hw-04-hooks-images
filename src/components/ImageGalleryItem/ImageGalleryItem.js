1178eb22f50b08874699480e987b7fa26f353d15
ms-anastasia <nastyakorn0@gmail.com>
2022-01-28 17:29:19 +0200

    update

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,19 +1,19 @@
-// import React from "react";
-// import PropTypes from "prop-types";
+import React from "react";
+import PropTypes from "prop-types";
 
-// import { ListItem, GalleryImage } from "./ImageGalleryItem.styled";
+import { ListItem, GalleryImage } from "./ImageGalleryItem.styled";
 
-// const GalleryItem = ({ webformatURL, largeImageURL, onClick }) => {
-//   return (
-//     <ListItem onClick={() => onClick(largeImageURL)}>
-//       <GalleryImage src={webformatURL} alt="" width = "200" height = "200"/>
-//     </ListItem>
-//   );
-// };
-// export default GalleryItem;
+const GalleryItem = ({ webformatURL, largeImageURL, onClick }) => {
+  return (
+    <ListItem onClick={() => onClick(largeImageURL)}>
+      <GalleryImage src={webformatURL} alt="" width = "200" height = "200"/>
+    </ListItem>
+  );
+};
+export default GalleryItem;
 
-// GalleryItem.propTypes = {
-//   webformatURL: PropTypes.string,
-//   largeImageURL: PropTypes.string,
-//   onClick: PropTypes.func,
-// };
+GalleryItem.propTypes = {
+  webformatURL: PropTypes.string,
+  largeImageURL: PropTypes.string,
+  onClick: PropTypes.func,
+};

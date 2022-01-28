6e473924b035f04472e2845ef882f4dcd36dfd23
ms-anastasia <nastyakorn0@gmail.com>
2022-01-28 17:28:44 +0200

    update

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,34 +1,34 @@
-import { useEffect } from "react";
-import { createPortal } from "react-dom";
+// import { useEffect } from "react";
+// import { createPortal } from "react-dom";
 
-import { ModalOverlay, ModalContent } from "./Modal.styled";
+// import { ModalOverlay, ModalContent } from "./Modal.styled";
 
-const modalRoot = document.querySelector("#modal-root");
+// const modalRoot = document.querySelector("#modal-root");
 
-export default function Modal({ onClose, children }) {
-  useEffect(() => {
-    window.addEventListener("keydown", handleKeyDown);
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [onClose]);
+// export default function Modal({ onClose, children }) {
+//   useEffect(() => {
+//     window.addEventListener("keydown", handleKeyDown);
+//     return () => {
+//       window.removeEventListener("keydown", handleKeyDown);
+//     };
+//   }, [onClose]);
 
-  const handleKeyDown = (e) => {
-    if (e.code === "Escape") {
-      onClose();
-    }
-  };
+//   const handleKeyDown = (e) => {
+//     if (e.code === "Escape") {
+//       onClose();
+//     }
+//   };
 
-  const handleBackdropClick = (event) => {
-    if (event.currentTarget === event.target) {
-      onClose();
-    }
-  };
+//   const handleBackdropClick = (event) => {
+//     if (event.currentTarget === event.target) {
+//       onClose();
+//     }
+//   };
 
-  return createPortal(
-    <ModalOverlay onClick={handleBackdropClick}>
-      <ModalContent>{children}</ModalContent>
-    </ModalOverlay>,
-    modalRoot
-  );
-}
+//   return createPortal(
+//     <ModalOverlay onClick={handleBackdropClick}>
+//       <ModalContent>{children}</ModalContent>
+//     </ModalOverlay>,
+//     modalRoot
+//   );
+// }

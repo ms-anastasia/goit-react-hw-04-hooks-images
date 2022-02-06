fd2efdf43b619f71876820f29ef6f9761c2852fb
Anastasia Korniichuk <81706073+ms-anastasia@users.noreply.github.com>
2022-02-06 15:41:35 +0200

    upd

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,25 +5,25 @@ import { ModalOverlay, ModalContent } from "./Modal.styled";
 
 const modalRoot = document.querySelector("#modal-root");
 
-export default function Modal({ onClose, children }) {
-  useEffect(() => {
-    window.addEventListener("keydown", handleKeyDown);
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [onClose]);
+// export default function Modal({ onClose, children }) {
+//   useEffect(() => {
+//     window.addEventListener("keydown", handleKeyDown);
+//     return () => {
+//       window.removeEventListener("keydown", handleKeyDown);
+//     };
+//   }, [onClose]);
 
-  const handleKeyDown = (e) => {
-    if (e.code === "Escape") {
-      onClose();
-    }
-  };
+//   const handleKeyDown = (e) => {
+//     if (e.code === "Escape") {
+//       onClose();
+//     }
+//   };
 
-  const handleBackdropClick = (event) => {
-    if (event.currentTarget === event.target) {
-      onClose();
-    }
-  };
+//   const handleBackdropClick = (event) => {
+//     if (event.currentTarget === event.target) {
+//       onClose();
+//     }
+//   };
 
   return createPortal(
     <ModalOverlay onClick={handleBackdropClick}>

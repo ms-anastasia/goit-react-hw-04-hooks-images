c35d6b6185e8f90083795cfeb7765806acb51255
Anastasia Korniichuk <81706073+ms-anastasia@users.noreply.github.com>
2022-02-06 15:42:23 +0200

    upd

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,21 +1,21 @@
-// import React from "react";
-// import { Gallery } from "./ImageGallery.styled";
-// import GalleryItem from "../ImageGalleryItem/ImageGalleryItem";
+import React from "react";
+import { Gallery } from "./ImageGallery.styled";
+import GalleryItem from "../ImageGalleryItem/ImageGalleryItem";
 
-// const ImageGallery = ({ images, onClick }) => {
-//   return (
-//     <div>
-//       <Gallery>
-//         {images.map(({ webformatURL, largeImageURL }, index) => (
-//           <GalleryItem
-//             key={index}
-//             webformatURL={webformatURL}
-//             largeImageURL={largeImageURL}
-//             onClick={onClick}
-//           />
-//         ))}
-//       </Gallery>
-//     </div>
-//   );
-// };
-// export default ImageGallery;
+const ImageGallery = ({ images, onClick }) => {
+  return (
+    <div>
+      <Gallery>
+        {images.map(({ webformatURL, largeImageURL }, index) => (
+          <GalleryItem
+            key={index}
+            webformatURL={webformatURL}
+            largeImageURL={largeImageURL}
+            onClick={onClick}
+          />
+        ))}
+      </Gallery>
+    </div>
+  );
+};
+export default ImageGallery;
